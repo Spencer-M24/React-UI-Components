64b600eee5734e0beae5b45de8d3fbec391c2040
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-11 12:31:14 -0800

    Change Js to JSX extention added the Import's of react then exported

---
+++ Project-I-Social-Card/social-card/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="FrontPageReact">
+    <HeaderContainer />
+    <CardContainer />
+    </div>
+
+  );
+};
+
+export default App;
